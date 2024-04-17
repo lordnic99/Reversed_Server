b6ff6bb785cb1f84fb166a3ea9724e79c329a095
Ubuntu <ubuntu@vps-24d0ac57.vps.ovh.ca>
2024-04-17 03:42:02 +0000

    update server (1)

--- run.py
+++ run.py
@@ -1,77 +1,47 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, jsonify, abort
-
-
+import secrets
+import os
 
 app = Flask(__name__)
 
 
-
-# Dummy data store and token for simplicity
-
-# In a real application, you should use a database and a secure method for tokens
-
-users = {}
-
-TOKEN = "hoang20cm"
-
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 
 def token_required(f):
-
     def decorator(*args, **kwargs):
-
         token = request.headers.get('Authorization')
-
-        if token != TOKEN:
-
-            abort(401, description="Invalid or missing token")
-
+        if token != SECRET_KEY:
+            return jsonify({"status": "NOK", "message": "Wrong secret key"}), 401
         return f(*args, **kwargs)
-
     decorator.__name__ = f.__name__
-
     return decorator
 
 
 
 @app.route('/createtunnel', methods=['POST'])
-
 @token_required
-
 def create_tunnel():
-
     data = request.json
-
     if not data or 'name' not in data:
-
         return jsonify({"error": "Name is required"}), 400
-
     name = data['name']
-
-    # For simplicity, we're just storing the name in a dictionary
-
     users[name] = f"Tunnel for {name} created"
-
     return jsonify({"message": f"Tunnel for {name} created successfully"}), 201
 
 
 
 @app.route('/gettunnel', methods=['GET'])
-
 @token_required
-
 def get_tunnel():
-
     name = request.args.get('name')
-
     if not name or name not in users:
-
         return "Tunnel not found", 404
-
     return users[name], 200
 
-
+users = {}
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', debug=True, port=45642)
